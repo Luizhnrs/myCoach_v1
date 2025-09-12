b7572c553def951591528f07b214d3356a387639
Luizhnrs <Luizhnrsg@gmail.com>
2025-09-11 21:28:54 -0300

    Feat: DB and Route config implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,13 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# Usuários de teste
+User.create!(name: "Aluno 1", email: "aluno1@example.com", password: "123456", role: "aluno")
+User.create!(name: "Aluno 2", email: "aluno2@example.com", password: "123456", role: "aluno")
+User.create!(name: "Mentor 1", email: "mentor1@example.com", password: "123456", role: "mentor")
+User.create!(name: "Mentor 2", email: "mentor2@example.com", password: "123456", role: "mentor")
+
+# Conexões de mentoria
+Mentorship.create!(mentor_id: User.find_by(email: "mentor1@example.com").id, student_id: User.find_by(email: "aluno1@example.com").id)
+Mentorship.create!(mentor_id: User.find_by(email: "mentor2@example.com").id, student_id: User.find_by(email: "aluno2@example.com").id)
